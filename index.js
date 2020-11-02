7caa9c0f030d17a61ff4e5ca07ea716874c9e334
I-Am-Lain <vetter.william@gmail.com>
2020-11-01 20:56:32 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name, email){
+
+
+    const configObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    }
+
+    // can also replace configObject with the declaration itself
+    return fetch('http://localhost:3000/users', configObject)
+    .then(resp => resp.json())
+    .then(function(json) {
+        const body = document.querySelector("body")
+
+        body.innerHTML = json.id
+    })
+    .catch(error => {
+        const body = document.querySelector("body")
+        body.innerHTML = error
+      })
+}
